5663c0821f7e40c1229c55cfeadbd585a18d82b3
allansteve <51958410+allansteve@users.noreply.github.com>
2019-07-27 18:31:15 -0300

    Implementação dos Hoocks

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,10 +16,14 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier'],
+  plugins: ['react', 'prettier', 'react-hoocks'],
   rules: {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'no-param-reassign': 'off',
     'import/prefer-default-export': 'off',
+    'no-console-': ['error', { allow: ['tron'] }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
